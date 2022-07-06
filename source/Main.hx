22c436a20eb203e621339ff0e698c4d5dd3dcde0
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-07-06 20:00:33 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -31,7 +31,7 @@ class Main extends Sprite
 	{
 		super();
 		
-              
+                SUtil.uncaughtErrorHandler
 		
 		if (stage != null)
 		{
@@ -67,7 +67,9 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		
+		SUtil.check();
+
+
 
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
